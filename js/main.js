4239c1b6bf040e143ae3f6e14cef657e396e5bb8
maksr51314 <maksr51314@gmail.com>
2014-07-17 18:14:02 +0300

    [game] added audio

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-
 'use strict'
 
 var SLOW_CONNECTION_LEVEL = 1;
 var IMAGE_PATH = 'img/';
 var Y_POSITION = '217';
+var imgs = [];
 
+var Rows = [72, 305, 550 ];
 var imagePaths = {
-    "BG.png" : {x: 0, y: 0},
-    "Bet_Line.png": {x: 0, y: 0},
-    "BTN_Spin.png": {x: 825, y: Y_POSITION},
-    "SYM1.png": {x: 200, y: 0},
-    "SYM3.png": {x: 300, y: Y_POSITION},
-    "SYM4.png": {x: 550, y: Y_POSITION},
-    "SYM5.png": {x: 305, y: Y_POSITION},
-    "SYM6.png": {x: 72, y: Y_POSITION},
-    "SYM7.png": {x: 0, y: Y_POSITION}
+    "BG.png" : {x: 0, y: 0, index: 0},
+    "BTN_Spin.png": {x: 825, y: Y_POSITION, index: 1},
+    "SYM1.png": {x: 0, y: 0, index: 1},
+    "SYM3.png": {x: 0, y: Y_POSITION, index: 1},
+    "SYM4.png": {x: 0, y: Y_POSITION, index: 1},
+    "SYM5.png": {x: 0, y: Y_POSITION, index: 1},
+    "SYM6.png": {x: 0, y: Y_POSITION, index: 1},
+    "SYM7.png": {x: 0, y: Y_POSITION, index: 1}
 };
 
 
@@ -30,20 +30,18 @@ loading.onload = function() {
 
 
 // Dynamically resize the canvas to be its CSS displayed size
-(window.onresize = function(){
+//(window.onresize = function(){
     can.width  = 960;
     can.height = 536;
-})();
+//})();
 
-//// Display a temporary loading message; will be erased by animation
-//ctx.textAlign    = 'center';
-//ctx.textBaseline = 'middle';
+//Function to play the exact file format
+function playAudio(){
+    var audio = new Audio("audio/example.wav");
+    audio.play();
+}
 
-
-// Load images and run the whenLoaded callback when all have loaded;
-// The callback is passed an array of loaded Image objects.
 function loadImages(paths, whenLoaded){
-    var imgs = [];
 
     paths.forEach(function(path){
         var img = new Image();
@@ -52,26 +50,102 @@ function loadImages(paths, whenLoaded){
             imgs.push(img);
 
             if (imgs.length == paths.length) {
-                whenLoaded(imgs);
+                whenLoaded(_.indexBy(imgs, 'name'));
             }
         };
 
         setTimeout(function(){
             img.src = IMAGE_PATH + path;
             img.name = path;
+            img.zIndex = imagePaths[path].index;
         }, SLOW_CONNECTION_LEVEL * 1000);
     });
 }
 
 function showImages(ctx, imgs) {
-    imgs.forEach(function(img) {
+    var images = _.values(imgs).sort(function(a, b) {return imagePaths[a.name].index - imagePaths[b.name].index});
+    images.forEach(function(img) {
         ctx.drawImage(img, imagePaths[img.name].x, imagePaths[img.name].y);
+    })
+}
+
+function addEvents() {
+    imgs[0].onkeydown = function() {
+      console.log('sss')
+    };
+
+    var playBtn = document.getElementById('playButton');
+    playBtn.addEventListener('click', function() {
+        console.log('clicked');
     })
 }
 
 loadImages(Object.keys(imagePaths), function(imgs){
     console.log('Loaded');
     ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
-    showImages(ctx, imgs)
+    addEvents(imgs);
+    showImages(ctx, imgs);
+
 });
+
+
+
+//
+//window.requestAnimFrame = (function(callback) {
+//    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+//        function(callback) {
+//            window.setTimeout(callback, 1000 / 60);
+//        };
+//})();
+//
+//function drawRectangle(myRectangle, context) {
+//    context.beginPath();
+//    context.rect(myRectangle.x, myRectangle.y, myRectangle.width, myRectangle.height);
+//    context.fillStyle = '#8ED6FF';
+//    context.fill();
+//    context.lineWidth = myRectangle.borderWidth;
+//    context.strokeStyle = 'black';
+//    context.stroke();
+//}
+//function animate(myRectangle, canvas, context, startTime) {
+//    // update
+//    var time = (new Date()).getTime() - startTime;
+//
+//    var linearSpeed = 100;
+//    // pixels / second
+//    var newX = linearSpeed * time / 1000;
+//
+//    if(newX < canvas.width - myRectangle.width - myRectangle.borderWidth / 2) {
+//        myRectangle.x = newX;
+//    }
+//
+//    // clear
+//    context.clearRect(0, 0, canvas.width, canvas.height);
+//
+//    drawRectangle(myRectangle, context);
+//
+//    // request new frame
+//    requestAnimFrame(function() {
+//        animate(myRectangle, canvas, context, startTime);
+//    });
+//}
+//var canvas = document.getElementById('myCanvas');
+//var context = canvas.getContext('2d');
+//
+//var myRectangle = {
+//    x: 0,
+//    y: 75,
+//    width: 100,
+//    height: 50,
+//    borderWidth: 5
+//};
+//
+//
+//setTimeout(function() {
+//    var startTime = (new Date()).getTime();
+//    animate(myRectangle, canvas, context, startTime);
+//}, 1000);
+
+
+//playAudio();
 
